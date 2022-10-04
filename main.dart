bade157877658b2ea2fc482b3bf3d418690bcadc
Usama Malik <113186340+usama-devloper@users.noreply.github.com>
2022-10-04 17:24:37 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -16,36 +16,47 @@ Widget build ( BuildContext context){
       return MaterialApp(
       debugShowCheckedModeBanner: false,
               home: Scaffold(
-               appBar: AppBar(
+               /*appBar: AppBar(
                 leading: Icon(Icons.menu, color: Colors.black),
                   title: Text("First_Application",textScaleFactor: 3,textDirection: TextDirection.ltr,),
                   
                   titleTextStyle: TextStyle(color: Colors.black), 
                   backgroundColor: Colors.limeAccent,      
-                ),
-                                              /* appBar:  AppBar(          //app baar copy from online
+                ),*/
+                                              appBar:  AppBar(          //app baar copy from online
                                               leading: Icon(Icons.menu),
-                                              title: Text('First_Application',textAlign: TextAlign.center,), 
+                                              title: Center (child: Text('First_Application',textAlign: TextAlign.center,
+                                              )
+                                              ), 
                                                 actions: [
                                                 Icon(Icons.favorite),
                                                 Padding(
                                                   padding: EdgeInsets.symmetric(horizontal: 16),
+                                                  
                                                   child: Icon(Icons.search),
                                                 ),
                                                 Icon(Icons.more_vert),
                                               ],
-                                              backgroundColor: Colors.purple,
-                                            ),*/
+                                              backgroundColor: Color.fromARGB(255, 26, 37, 43),
+                                            ),
 
                       body: Container(
-                        height: 200,
+                        height: 100,
                         width: 3000,
-                        margin: EdgeInsets.all(20),
+                        margin: EdgeInsets.all(30),
                         alignment: Alignment.topCenter,
-                       // padding: EdgeInsets.only(right: 20),
-                        child: Text("Welcome to My First App",textScaleFactor: 2,),
+                        padding: EdgeInsets.only(right: 20),
+                        child: Text("Welcome to My First App",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+
+                        ),
+                        
+                        textScaleFactor: 2,),
                                     decoration: BoxDecoration(
-                                      color: Colors.lightGreen,
+                                      
+                                      color: Color.fromARGB(255, 23, 55, 70),
                                       boxShadow: [
                                       BoxShadow (blurRadius: 260.0,color: Colors.deepPurple),
                                         ],
